ea82e5a16a7d9a8ffc91faeb7be0023b86d90b58
NICHAPA <nichapa.por@dome.tu.ac.th>
2024-03-19 20:21:44 +0700

    Edit pop up in loginpage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,27 @@
-import { useEffect, useState } from 'react';
-import '/src/styles/LoginPage.css';
-import HeadPic from '/src/assets/vidvabuilding.jpg';
-import LogoPic from '/src/assets/EnBookingLogo.jpeg'
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { Password } from 'primereact/password';
-        
-import api from '/src/api.jsx'
+import React, { useEffect, useState } from "react";
+import "/src/styles/LoginPage.css";
+import HeadPic from "/src/assets/vidvabuilding.jpg";
+import LogoPic from "/src/assets/EnBookingLogo.jpeg";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import api from "/src/api.jsx";
+
+const AlertPopup = React.forwardRef((props, ref) => (
+  <Stack sx={{ width: "80%" }}>
+    <Alert variant="outlined" severity="error">
+    {props.children}
+    </Alert>
+  </Stack>
+));
 
 const LoginForm = () => {
-  const [student_info_id, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [student_info_id, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [loginStatus, setLoginStatus] = useState("");
   const navigate = useNavigate();
-
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -33,78 +38,84 @@ const LoginForm = () => {
     // },[]);
     // ตรวจสอบการ log in กับ backend
     try {
-      const response = await api.get(`/signup/log_in?users_id=${student_info_id}&password=${password}`, loginData)
+      const response = await api.get(
+        `/signup/log_in?users_id=${student_info_id}&password=${password}`,
+        loginData
+      );
       if (response.status === 200) {
-        alert("Login successfully!")
-        console.log('response: ', response.data);
-        // เก็บ student_info_id ไว้ใน localStorage
+        // alert("Login successfully!");
+        setLoginStatus("200");
         localStorage.setItem('student_info_id',student_info_id);
         console.log('User id: ',localStorage.student_info_id);
-        // ไปหน้า homepage
         navigate('/homepage');
       } else {
+        setLoginStatus(response.data.message)
         setErrors({ global: response.data.message });
       }
-    }
-    catch (error) {
-      console.error('Signup error:', error);
+    } catch (error) {
+      console.error("Signup error:", error);
       if (error.response && error.response.data && error.response.data.detail) {
         // alert ข้อความจาก backend
-        alert(`Error: ${error.response.data.detail}`);
+        setLoginStatus(`Error: ${error.response.data.detail}`)
+        // alert(`Error: ${error.response.data.detail}`);
       } else {
         alert("An error occurred during signup. Please try again.");
+        setLoginStatus("An error occurred during signup. Please try again.")
       }
-    }     
+    }
   };
 
   // log in page
   return (
-    <div className='wrapper'>
-        <form onSubmit={handleLogin}>
+    <div className="wrapper">
+      <form onSubmit={handleLogin}>
+        <div className="head-container">
+          <img src={HeadPic} alt="Headpic" className="head-pic" />
+        </div>
 
-            <div className="head-container">
-              <img src={HeadPic} alt="Headpic" className="head-pic"/>
-            </div>
+        <div className="login-container">
+          <img src={LogoPic} alt="LogoPic" className="logo-pic" />
 
-            <div className="login-container">
-              <img src={LogoPic} alt="LogoPic" className="logo-pic"/>
-              <div className="input-box1">
-                <input
-                  type="number"
-                  placeholder='Student ID'
-                  required
-                  value={student_info_id}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
+          <div className="input-box1">
+            <input
+              type="number"
+              placeholder="Student ID"
+              required
+              value={student_info_id}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
 
-              <div className="input-box2">
-                <input
-                  type="text"
-                  className='input-pwd'
-                  name="input-pwd"
-                  placeholder='Password'
-                  required
-                  value={password}
-                  minLength={8}
-                  maxLength={24}
-                  onChange={(e) => setPassword(e.target.value)}
-                  autoComplete="current-password"
-                />
-                
-                 
-              </div>
+          <div className="input-box2">
+            <input
+              type="text"
+              className="input-pwd"
+              name="input-pwd"
+              placeholder="Password"
+              required
+              value={password}
+              minLength={8}
+              maxLength={24}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+          </div>
 
-              <div className="forgot">
-                <p>Do not have an account? <Link to="/signup">Click here to sign up</Link> </p>
-              </div>
+          <div className="forgot">
+            <p>
+              Do not have an account?{" "}
+              <Link to="/signup">Click here to sign up</Link>{" "}
+            </p>
+          </div>
 
-              <button className='submit-btn'type='submit'>Sign In</button>
-            </div>
-
-        </form>
+          <button className="submit-btn" type="submit">
+            Sign In
+          </button>
+          { loginStatus !== "" && loginStatus !== '200' ? <AlertPopup> {loginStatus} </AlertPopup> : ""}
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default LoginForm;
